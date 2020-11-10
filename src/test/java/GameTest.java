ffecb8287b26bf552da38c5593cd954661a3db47
mateuszkowalczykk <x625134x@gmail.com>
2020-11-10 17:01:39 +0100

    {RED}can get score

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -7,6 +7,12 @@ import org.junit.jupiter.api.Test;
 public class GameTest {
     Game game;
 
+    void multiRoll(int... ints){
+        for(int i : ints){
+            game.roll(i);
+        }
+    }
+
     @BeforeEach
     void setUp(){
         game = new Game();
@@ -15,5 +21,10 @@ public class GameTest {
     @Test
     void canRoll() {
         game.roll(0);
+    }
+    @Test
+    void canScore() {
+        multiRoll(0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+        game.score();
     }
 }
