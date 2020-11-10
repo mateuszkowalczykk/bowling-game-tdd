abc9eb27874c6061f86a73c75f93169aa4cc0ed9
mateuszkowalczykk <x625134x@gmail.com>
2020-11-10 16:46:29 +0100

    init project

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,15 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class GameTest {
+    Game game;
+
+    @BeforeEach
+    void setUp(){
+        game = new Game();
+    }
+
+}
