6612d0dde67dbfb3b22b87e00dcaa9f2e8ee3edb
mateuszkowalczykk <x625134x@gmail.com>
2020-11-10 17:33:53 +0100

    {REFACTOR}handle spare

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -10,12 +10,17 @@ public class Game {
         int result = 0;
         int rollNumber = 0;
         for (int i = 1; i <= 10; i++) {
-            result += rolls[rollNumber] + rolls[rollNumber+1];
-            if(rolls[rollNumber] + rolls[rollNumber+1]==10){//spare
-                result+=rolls[rollNumber+2];
+
+            if(isSpare(rollNumber)){
+                result+= 10 + rolls[rollNumber+2];
+            }else{
+                result += rolls[rollNumber] + rolls[rollNumber+1];
             }
             rollNumber+= 2;
         }
         return result;
+    }
+    private boolean isSpare(int rollNumber){
+        return rolls[rollNumber] + rolls[rollNumber+1] == 10 && rolls[rollNumber] !=10 && rolls[rollNumber] !=0;
     }
 }
